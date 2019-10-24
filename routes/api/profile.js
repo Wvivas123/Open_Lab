90449400ac9b56f6d963fa093a4a4fec6d8666bb
William Vivas <williamvivasdev@gmail.com>
2019-10-24 16:07:53 -0400

    adding user profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const User = require('../../models/Users');
 //@route  Get api/profile/myprofile 
 //@desc   Get Current users Profile 
 //@access Private   
 
-router.get('/', auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({user: req.user.id}).populate();
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user',
+            ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'There is no profile for this user'
+            });
+        }
 
     } catch (err) {
         console.error(err.message);
@@ -19,6 +32,53 @@ router.get('/', auth, async (req, res) => {
 });
 
 
+
+//@route  POST api/profile/me
+//@desc   Create or update user profile 
+//@acess  Private 
+
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills are required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+
+    } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+        profileFields.skills = skills.split(',').map()
+    }
+
+
+
+});
 
 
 module.exports = router;
